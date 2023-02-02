d1c998c3181af9260f342e13de3dbc32c19bcc93
Mazylol <landonp95@gmail.com>
2023-02-02 18:21:30 -0500

    basic bot

--- Program.cs
+++ Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DotNetEnv;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
 
-Console.WriteLine("Hello, World!");
+namespace WeatherCord
+{
+    class Program
+    {
+        static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            Env.TraversePath().Load();
+            var discordToken = Env.GetString("DEV_TOKEN");
+            var guildId = Convert.ToUInt64(Env.GetString("GUILD_ID"));
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = discordToken,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            discord.Ready += DiscordReady;
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task DiscordReady(DiscordClient client, ReadyEventArgs e)
+        {
+            await client.UpdateStatusAsync(new DiscordActivity("no storms?"), UserStatus.Online);
+        }
+    }
+}
